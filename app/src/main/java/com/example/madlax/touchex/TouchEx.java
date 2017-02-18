3bbf9dd776b031f0202e54f09c484c27286dc800
aglio26 <madlax@akasiyaki.local>
2017-02-18 20:50:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/madlax/touchex/TouchEx.java
@@ -0,0 +1,17 @@
+package com.example.madlax.touchex;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+/**
+ * Created by madlax on 2017/02/17.
+ */
+
+public class TouchEx extends Activity{
+    //起動処理
+    @Override
+    public void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new TouchView(this));
+    }
+}
